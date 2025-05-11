612ba3c0eaddf9123539b3899215cd2519458e30
Burak Bıçakcıoğlu <106585943+burakbicakci@users.noreply.github.com>
2025-05-11 14:27:45 +0300

    refactor: Remove NFTDetailsPage component and update donations slice to improve state management and API request methods

--- src/pages/admin/nft-list/[id].js
+++
@@ -1,9 +0,0 @@
-import NFTDetails from "@/views/admin/nft-list/NFTDetails"
-
-const NFTDetailsPage = () => <NFTDetails />
-
-NFTDetailsPage.acl = {
-    action: 'read',
-    permission: 'admin'
-}
-export default NFTDetailsPage 
